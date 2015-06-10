9c4b354b01894620f477975952427f75881ec68d
joshuarossi <joshua.rossi@icloud.com>
2015-06-10 15:52:53 -0500

    added github to package.js

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Wrapper for Blockchain.info websocket data stream',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/joshuarossi/meteor.blockchain.info.websocket.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
